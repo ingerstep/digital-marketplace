65b2b590148706cd70a50c830ca1ad5346f7378c
Stepan <stepan.karelin.1996@mail.ru>
2024-05-20 19:35:59 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1>
+          Your marketplace for high-quality{" "}
+          <span className="text-blue-600">digital assets</span>
+        </h1>
+      </div>
+    </MaxWidthWrapper>
+  );
+}
