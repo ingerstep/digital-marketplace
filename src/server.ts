7691572ca507f1eceb59353de5384e1432ff27dd
Stepan <stepan.karelin.1996@mail.ru>
2024-05-21 07:56:42 +0700

    Add trpc to backend

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from "express"
 import { getPayloadClient } from "./get-payload"
 import { nextApp, nextHandler } from "./next-utils"
+import * as trpcExpress from "@trpc/server/adapters/express"
+import { appRouter } from "./trpc"
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
+
+const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
+    req, res
+})
 
 const start = async () => {
     const payload = await getPayloadClient({
@@ -15,12 +21,17 @@ const start = async () => {
         }
     })
 
+    app.use('/api/trpc', trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    }))
+
     app.use((req, res) => nextHandler(req, res))
 
-    nextApp.prepare().then(()=> {
+    nextApp.prepare().then(() => {
         payload.logger.info('Next.js started')
 
-        app.listen(PORT, async ()=> {
+        app.listen(PORT, async () => {
             payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
         })
     })
